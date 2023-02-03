86aafcf47078b89566fa75181d47439de76212e3
hieuneko <hieuneko@gmail.com>
2023-02-03 15:31:02 +0700

    fix variable sohoc 03

--- src/main/java/com/phamhieu/exercise/basic/number/Sohoc03.java
+++ src/main/java/com/phamhieu/exercise/basic/number/Sohoc03.java
@@ -2,21 +2,21 @@ package com.phamhieu.exercise.basic.number;
 
 public class Sohoc03 {
 
-    public String findTheProductOfPrimeFactors(final int x) {
-        int n = x;
+    public String findTheProductOfPrimeFactors(final int input) {
+        int number = input;
         int i = 2;
         final StringBuilder arrayPrime = new StringBuilder();
-        if (n == 0 || n == 1) {
+        if (number == 0 || number == 1) {
             return "0";
         }
-        if (n < 0) {
-            return "- " + findTheProductOfPrimeFactors(-n);
+        if (number < 0) {
+            return "- " + findTheProductOfPrimeFactors(-number);
         }
-        while (n > 1) {
+        while (number > 1) {
             if (checkPrime(i)) {
-                if (n % i == 0) {
+                if (number % i == 0) {
                     arrayPrime.append(i).append(" * ");
-                    n /= i;
+                    number /= i;
                 } else
                     i++;
             } else
@@ -25,12 +25,12 @@ public class Sohoc03 {
         return arrayPrime.substring(0, arrayPrime.length() - 2).trim();
     }
 
-    public boolean checkPrime(final int x) {
-        if (x < 2) {
+    public boolean checkPrime(final int input) {
+        if (input < 2) {
             return false;
         }
-        for (int i = 2; i <= (int) Math.sqrt(x); i++) {
-            if (x % i == 0) {
+        for (int i = 2; i <= (int) Math.sqrt(input); i++) {
+            if (input % i == 0) {
                 return false;
             }
         }

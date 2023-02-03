c97959a3a95d17dbc96a0ca86c01006c466e3b59
hieuneko <hieuneko@gmail.com>
2023-02-03 10:15:59 +0700

    fix convention sohoc 02

--- src/main/java/com/phamhieu/exercise/basic/number/Sohoc02.java
+++ src/main/java/com/phamhieu/exercise/basic/number/Sohoc02.java
@@ -2,9 +2,10 @@ package com.phamhieu.exercise.basic.number;
 
 public class Sohoc02 {
 
-    public int findTotalOfNumber(final int n){
-        int input = n, total = 0;
-        while (input != 0){
+    public int findTotalOfNumber(final int n) {
+        int input = n;
+        int total = 0;
+        while (input != 0) {
             total += input % 10;
             input /= 10;
         }

1f831bf843decf7ecd404a16c406d88b85c62caa
hieuneko <hieuneko@gmail.com>
2023-02-03 15:33:18 +0700

    fix variable sohoc 05

--- src/main/java/com/phamhieu/exercise/basic/number/Sohoc05.java
+++ src/main/java/com/phamhieu/exercise/basic/number/Sohoc05.java
@@ -3,13 +3,13 @@ package com.phamhieu.exercise.basic.number;
 public class Sohoc05 {
 
     //Viết phương trình nhập vào n và in ra tổng của dãy số Dãy số 1*2 + 2*3 + 3*4 +...+ n*(n+1)
-    public int findSum(final int x) {
+    public int findSum(final int input) {
         int total = 0;
-        if(x <= 0){
+        if(input <= 0){
             return 0;
         }
 
-        for (int i = 1; i <= x; i++) {
+        for (int i = 1; i <= input; i++) {
             total += i * (i + 1);
         }
         return total;

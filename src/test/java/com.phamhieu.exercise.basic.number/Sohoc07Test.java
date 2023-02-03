3ec6fde267df711376e19c2859d3ee8c46272278
hieuneko <hieuneko@gmail.com>
2023-02-03 14:53:59 +0700

    fix test sohoc 08

--- src/test/java/com.phamhieu.exercise.basic.number/Sohoc07Test.java
+++ src/test/java/com.phamhieu.exercise.basic.number/Sohoc07Test.java
@@ -10,6 +10,7 @@ class Sohoc07Test {
     void checkArmstrongNumber() {
         final var test = new Sohoc07();
         assertEquals(false, test.checkArmstrongNumber(0));
+        assertEquals(false, test.checkArmstrongNumber(100000));
         assertEquals(false, test.checkArmstrongNumber(-2));
         assertEquals(true, test.checkArmstrongNumber(8208));
         assertEquals(false,test.checkArmstrongNumber(1234));

cbfd0913d70daf43e831d4181e0430ee2a5f8727
hieuneko <hieuneko@gmail.com>
2023-02-03 15:32:31 +0700

    fix variable sohoc 04

--- src/main/java/com/phamhieu/exercise/basic/number/Sohoc04.java
+++ src/main/java/com/phamhieu/exercise/basic/number/Sohoc04.java
@@ -4,20 +4,20 @@ import java.util.ArrayList;
 
 public class Sohoc04 {
 
-    public int findTotalOfArrayFibonaci(final int x) {
+    public int findTotalOfArrayFibonaci(final int input) {
         int total = 0;
         ArrayList<Integer> arrayFibonaci = new ArrayList<>();
         arrayFibonaci.add(1);
         arrayFibonaci.add(1);
-        if (x <= 0) {
+        if (input <= 0) {
             return 0;
         }
 
-        if (x == 1) {
+        if (input == 1) {
             return 1;
         }
 
-        while (arrayFibonaci.get(arrayFibonaci.size() - 1) < x) {
+        while (arrayFibonaci.get(arrayFibonaci.size() - 1) < input) {
             int len = arrayFibonaci.size();
             arrayFibonaci.add(arrayFibonaci.get(len - 1) + arrayFibonaci.get(len - 2));
         }

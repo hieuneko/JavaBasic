b4ef2238f424f67c0469c0fa66929a5e108d02ee
hieuneko <hieuneko@gmail.com>
2023-02-07 14:52:59 +0700

    array 07

--- src/main/java/com/phamhieu/exercise/basic/array/Array07.java
+++ src/main/java/com/phamhieu/exercise/basic/array/Array07.java
@@ -19,6 +19,13 @@ public class Array07 {
 
     private static int getNumberPosition(final int[] input, final int number) {
         int position = 0;
+        if (input.length == 0) {
+            return 0;
+        }
+        
+        if (input[input.length - 1] < number) {
+            return input.length;
+        }
         for (int i = 1; i < input.length; i++) {
             if (input[i] >= number && input[i - 1] <= number) {
                 position = i;
--- src/test/java/com/phamhieu/exercise/basic/array/Array07Test.java
+++ src/test/java/com/phamhieu/exercise/basic/array/Array07Test.java
@@ -9,6 +9,7 @@ class Array07Test {
     @Test
     void insertNumberToArray() {
         final var test = new Array07();
+        assertArrayEquals(new int[]{1, 2, 3, 5, 6, 7}, test.insertNumberToArray(new int[]{1, 2, 3, 5, 6}, 7));
         assertArrayEquals(new int[]{1, 2, 3, 5, 6}, test.insertNumberToArray(new int[]{1, 2, 5, 6}, 3));
         assertArrayEquals(new int[]{4, 5, 5, 14, 16}, test.insertNumberToArray(new int[]{4, 5, 14, 16}, 5));
         assertArrayEquals(new int[]{1}, test.insertNumberToArray(new int[]{}, 1));

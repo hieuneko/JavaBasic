69391e310723cb4a584b20dd2586fe7afc07720e
hieuneko <hieuneko@gmail.com>
2023-02-07 16:51:56 +0700

    fix convention array 07

--- src/main/java/com/phamhieu/exercise/basic/array/Array07.java
+++ src/main/java/com/phamhieu/exercise/basic/array/Array07.java
@@ -2,6 +2,9 @@ package com.phamhieu.exercise.basic.array;
 
 public class Array07 {
 
+    //Viết chương trình chèn một số nguyên x vào một mảng số nguyên đã sắp
+    // xếp theo thứ tự tăng dần mà vẫn đảm bảo mảng kết quả tăng dần
+    // (yêu cầu không thực hiện thao tác sắp xếp trong hàm)
     public int[] insertNumberToArray(final int[] input, final int number) {
         int[] result = new int[input.length + 1];
         int j = 0;

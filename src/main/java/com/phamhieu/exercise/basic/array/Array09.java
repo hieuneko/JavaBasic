3666fa893161c697a830b2e70be39fe7dacc6c67
hieuneko <hieuneko@gmail.com>
2023-02-07 17:29:32 +0700

    fix solution array 09

--- src/main/java/com/phamhieu/exercise/basic/array/Array09.java
+++ src/main/java/com/phamhieu/exercise/basic/array/Array09.java
@@ -22,7 +22,7 @@ public class Array09 {
             return false;
         }
 
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i < n / 2; i++) {
             if (n % i == 0) {
                 return false;
             }

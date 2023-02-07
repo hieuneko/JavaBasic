bf289921baa35bb8f9e6245fb29f8e903ff7c0bd
hieuneko <hieuneko@gmail.com>
2023-02-07 08:31:05 +0700

    fix for test sohoc 03

--- src/main/java/com/phamhieu/exercise/basic/number/Sohoc03.java
+++ src/main/java/com/phamhieu/exercise/basic/number/Sohoc03.java
@@ -4,14 +4,15 @@ public class Sohoc03 {
 
     public String findTheProductOfPrimeFactors(final int input) {
         int number = input;
-        int i = 2;
-        final StringBuilder arrayPrime = new StringBuilder();
         if (number == 0 || number == 1) {
             return "0";
         }
         if (number < 0) {
             return "- " + findTheProductOfPrimeFactors(-number);
         }
+
+        final StringBuilder arrayPrime = new StringBuilder();
+        int i = 2;
         while (number > 1) {
             if (checkPrime(i)) {
                 if (number % i == 0) {
@@ -26,9 +27,6 @@ public class Sohoc03 {
     }
 
     public boolean checkPrime(final int input) {
-        if (input < 2) {
-            return false;
-        }
         for (int i = 2; i <= (int) Math.sqrt(input); i++) {
             if (input % i == 0) {
                 return false;

49835b22deec2dc01fd4e0cffbf6f31a32cfd918
hieuneko <hieuneko@gmail.com>
2023-02-08 09:30:11 +0700

    fix solution array 07

--- src/main/java/com/phamhieu/exercise/basic/array/Array07.java
+++ src/main/java/com/phamhieu/exercise/basic/array/Array07.java
@@ -6,35 +6,33 @@ public class Array07 {
     // xếp theo thứ tự tăng dần mà vẫn đảm bảo mảng kết quả tăng dần
     // (yêu cầu không thực hiện thao tác sắp xếp trong hàm)
     public int[] insertNumberToArray(final int[] input, final int number) {
+        if (input.length == 0) {
+            return new int[]{number};
+        }
+
         int[] result = new int[input.length + 1];
-        int j = 0;
-        int position = getNumberPosition(input, number);
-        for (int i = 0; i < result.length; i++) {
-            if (i == position) {
+        if (number <= input[0]) {
+            result[0] = number;
+        } else {
+            result[0] = input[0];
+        }
+        int length = result.length;
+        if (number >= input[input.length - 1]) {
+            result[input.length] = number;
+            length = length - 1;
+        }
+        int j = 1;
+        boolean check = false;
+        for (int i = 1; i < length; i++) {
+            if (input[j] >= number && input[j - 1] <= number && !check) {
                 result[i] = number;
-            } else {
-                result[i] = input[j];
-                j++;
+                check = true;
+                i++;
             }
+            result[i] = input[j];
+            j++;
+
         }
         return result;
-    }
-
-    private static int getNumberPosition(final int[] input, final int number) {
-        if (input.length == 0) {
-            return 0;
-        }
-
-        if (input[input.length - 1] < number) {
-            return input.length;
-        }
-
-        int position = 0;
-        for (int i = 1; i < input.length; i++) {
-            if (input[i] >= number && input[i - 1] <= number) {
-                position = i;
-            }
-        }
-        return position;
     }
 }

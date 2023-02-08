403197230d525a76ba9fcbcad159efa5c16cc679
hieuneko <hieuneko@gmail.com>
2023-02-08 14:31:31 +0700

    fix solution for sohoc 05

--- src/main/java/com/phamhieu/exercise/basic/number/Sohoc05.java
+++ src/main/java/com/phamhieu/exercise/basic/number/Sohoc05.java
@@ -4,11 +4,11 @@ public class Sohoc05 {
 
     //Viết phương trình nhập vào n và in ra tổng của dãy số Dãy số 1*2 + 2*3 + 3*4 +...+ n*(n+1)
     public int findSum(final int input) {
-        int total = 0;
-        if(input <= 0){
+        if (input <= 0) {
             return 0;
         }
 
+        int total = 0;
         for (int i = 1; i <= input; i++) {
             total += i * (i + 1);
         }

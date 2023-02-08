5551fae459befcf24476eb10eb6df93a38728cd0
Võ Quang Hòa <voquanghoa@gmail.com>
2023-02-08 10:06:59 +0700

    Update src/test/java/com.phamhieu.exercise.basic.number/Sohoc09Test.java

--- src/test/java/com.phamhieu.exercise.basic.number/Sohoc09Test.java
+++ src/test/java/com.phamhieu.exercise.basic.number/Sohoc09Test.java
@@ -13,6 +13,6 @@ class Sohoc09Test {
         assertEquals(25D, test.findSquareRoot(625), 0.0001);
         assertEquals(3.464101, test.findSquareRoot(12), 0.0001);
         assertEquals(0.1, test.findSquareRoot(0.01), 0.0001);
-        assertEquals(0.5, test.findSquareRoot(0.25), 0.0001);
+        assertEquals(0.7071, test.findSquareRoot(0.5), 0.0001);
     }
 }

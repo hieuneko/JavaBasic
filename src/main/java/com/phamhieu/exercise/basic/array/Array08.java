b829e401a452cab7cb3d3238398fac4809e299fd
hieuneko <hieuneko@gmail.com>
2023-02-08 09:36:01 +0700

    fix solution array 08

--- src/main/java/com/phamhieu/exercise/basic/array/Array08.java
+++ src/main/java/com/phamhieu/exercise/basic/array/Array08.java
@@ -4,10 +4,6 @@ public class Array08 {
 
     //Viết chương trình tính tổng các bình phương của các phần tử
     public int findSumSquaresArray(final int[] input) {
-        if (input.length == 0) {
-            return 0;
-        }
-
         int sum = 0;
         for (int i : input) {
             sum += i * i;

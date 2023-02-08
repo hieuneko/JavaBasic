4c931f8540a3a42d48bef25b8847697525792ce2
hieuneko <hieuneko@gmail.com>
2023-02-08 11:03:32 +0700

    fix solution for sohoc04

--- src/main/java/com/phamhieu/exercise/basic/number/Sohoc04.java
+++ src/main/java/com/phamhieu/exercise/basic/number/Sohoc04.java
@@ -1,14 +1,8 @@
 package com.phamhieu.exercise.basic.number;
 
-import java.util.ArrayList;
-
 public class Sohoc04 {
 
     public int findTotalOfArrayFibonaci(final int input) {
-        int total = 0;
-        ArrayList<Integer> arrayFibonaci = new ArrayList<>();
-        arrayFibonaci.add(1);
-        arrayFibonaci.add(1);
         if (input <= 0) {
             return 0;
         }
@@ -17,12 +11,15 @@ public class Sohoc04 {
             return 1;
         }
 
-        while (arrayFibonaci.get(arrayFibonaci.size() - 1) < input) {
-            int len = arrayFibonaci.size();
-            arrayFibonaci.add(arrayFibonaci.get(len - 1) + arrayFibonaci.get(len - 2));
-        }
-        for(int num : arrayFibonaci) {
-            total = total + num;
+        int total = 2;
+        int f0;
+        int f1 = 1;
+        int fn = 1;
+        while (fn < input) {
+            f0 = f1;
+            f1 = fn;
+            fn = f0 + f1;
+            total += fn;
         }
         return total;
     }

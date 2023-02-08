fed2541079a3b6b9eb2d295f66ddcb6c50f70ec6
hieuneko <hieuneko@gmail.com>
2023-02-08 09:58:23 +0700

    fix solution sohoc09 for new test

--- src/main/java/com/phamhieu/exercise/basic/number/Sohoc09.java
+++ src/main/java/com/phamhieu/exercise/basic/number/Sohoc09.java
@@ -6,11 +6,11 @@ public class Sohoc09 {
     private static final double ACCURACY = 0.0001;
 
     public double findSquareRoot(final double input) {
-        if (input < 2) {
-            return input;
+        if (input < 1 && input > 0) {
+            return findSquareRoot(input * 100) / 10;
         }
 
-        double start = 1;
+        double start = 0;
         double end = input;
         while (end - start >= ACCURACY) {
             final double mid = (start + end) / 2;

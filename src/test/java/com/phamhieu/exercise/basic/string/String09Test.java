b06e6725837289c3b9f103accfdcabe64f7da164
hieuneko <hieuneko@gmail.com>
2023-02-09 11:34:01 +0700

    string 10

--- src/test/java/com/phamhieu/exercise/basic/string/String09Test.java
+++ src/test/java/com/phamhieu/exercise/basic/string/String09Test.java
@@ -9,7 +9,7 @@ class String09Test {
     @Test
     void formatCharInString() {
         final var test = new String09();
-        assertEquals("Chao Ca.c Ban", test.formatCharInString("CHao cA.C Ban"));
+        assertEquals("Chao Cac . Ban", test.formatCharInString("CHao cAC . Ban"));
         assertEquals("Viet Chuong Trinh De Format", test.formatCharInString("vIEt ChUOnG tRINH de foRmat"));
     }
 }

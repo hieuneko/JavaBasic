8fb322d7a9e64ad085b042ae5d09096dddd62a58
hieuneko <hieuneko@gmail.com>
2023-02-09 13:47:46 +0700

    temp

--- src/main/java/com/phamhieu/exercise/basic/string/String04.java
+++ src/main/java/com/phamhieu/exercise/basic/string/String04.java
@@ -2,13 +2,9 @@ package com.phamhieu.exercise.basic.string;
 
 public class String04 {
     public boolean checkDuplicateString(final String input) {
-        for (int i = 0; i < input.length() - 2; i++) {
-            for (int j = i + 2; j < input.length() - 1; j++) {
-                if (input.charAt(i) == input.charAt(j) && input.charAt(i + 1) == input.charAt(j + 1)) {
-                    return true;
-                }
-            }
+        int count = 0;
+        for (int i = 0;i<input.length();i++){
+            if (in)
         }
-        return false;
     }
 }

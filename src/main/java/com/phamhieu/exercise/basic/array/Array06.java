d9268039223ac7c2c1772073054cf08c716726a9
hieuneko <hieuneko@gmail.com>
2023-02-14 10:44:07 +0700

    fix solution for array 06

--- src/main/java/com/phamhieu/exercise/basic/array/Array06.java
+++ src/main/java/com/phamhieu/exercise/basic/array/Array06.java
@@ -2,11 +2,7 @@ package com.phamhieu.exercise.basic.array;
 
 public class Array06 {
 
-    //Viết chương trình sử dụng phương pháp tìm kiếm nhị phân để tìm kiếm
-    // một số nguyên từ một mảng số nguyên đã sắp xếp (tăng dần hoặc giảm dần),
-    // trả về vị trí nếu tìm thấy hoặc -1 nếu không tìm thấy
-    //Tìm kiếm cho mảng tăng dần
-    public int findLocationNumber(final int[] input, int number) {
+    public int findLocationNumber(final int[] input, final int number) {
         int right = input.length - 1;
         int left = 0;
         while (right >= left) {

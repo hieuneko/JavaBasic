f23a8e27376f346b3f55e30328dc1d53fbf96c0f
hieuneko <hieuneko@gmail.com>
2023-02-14 11:30:27 +0700

    temp

--- src/main/java/com/phamhieu/exercise/basic/number/Sohoc06.java
+++ src/main/java/com/phamhieu/exercise/basic/number/Sohoc06.java
@@ -3,13 +3,14 @@ package com.phamhieu.exercise.basic.number;
 public class Sohoc06 {
 
     static final double ACCURACY = 0.0001;
-    public double findSquareRoot(final double input){
+
+    public double findSquareRoot(final double input) {
         double number = input;
         double temp = 1;
         while (number - temp > ACCURACY) {
             number = (number + temp) / 2;
             temp = input / number;
         }
-        return Math.round(number * 1000000.0) / 1000000.0;
+        return number;
     }
 }
--- src/test/java/com.phamhieu.exercise.basic.number/Sohoc06Test.java
+++ src/test/java/com.phamhieu.exercise.basic.number/Sohoc06Test.java
@@ -2,14 +2,14 @@ package com.phamhieu.exercise.basic.number;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Sohoc06Test {
 
     @Test
     void findSquareRoot() {
         final var test = new Sohoc06();
-        assertEquals(1, test.findSquareRoot(1));
-        assertEquals(7.071068, test.findSquareRoot(50));
+        assertEquals(1, test.findSquareRoot(1), 0.000001);
+        assertEquals(7.071068, test.findSquareRoot(50), 0.000001);
     }
 }

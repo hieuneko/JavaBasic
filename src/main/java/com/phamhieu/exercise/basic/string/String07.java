6e00c84cc4c86bd75ce2daba9c26c5b01e14eac7
hieuneko <hieuneko@gmail.com>
2023-02-14 14:17:27 +0700

    fix solution for string 07

--- src/main/java/com/phamhieu/exercise/basic/string/String07.java
+++ src/main/java/com/phamhieu/exercise/basic/string/String07.java
@@ -3,16 +3,26 @@ package com.phamhieu.exercise.basic.string;
 public class String07 {
 
     public int findIndexContainString(final String input, final String subString) {
-        for (int i = 0; i <= input.length() - subString.length(); i++) {
-            int j = 0;
-            while (j < subString.length() && input.charAt(i + j) == subString.charAt(j)) {
-                j++;
-            }
-            if (j == subString.length()) {
+        for (int i = 0; i < input.length(); i++) {
+            if (isStringFoundAt(input, subString, i)) {
                 return i;
             }
         }
         return -1;
+    }
+
+    private static boolean isStringFoundAt(String input, String subString, int index) {
+        if (index + subString.length() > input.length()) {
+            return false;
+        }
+
+        for (int i = 0; i < subString.length(); i++) {
+            if (input.charAt(index + i) != subString.charAt(i)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
 

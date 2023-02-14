274df2632c35ec007894135df3f6909693d9a0da
hieuneko <hieuneko@gmail.com>
2023-02-14 10:11:33 +0700

    fix solution for string 03

--- src/main/java/com/phamhieu/exercise/basic/string/String03.java
+++ src/main/java/com/phamhieu/exercise/basic/string/String03.java
@@ -6,12 +6,10 @@ public class String03 {
         int number = 0;
         for (int i = 0; i < input.length(); i++) {
             if (input.charAt(i) >= '0' && input.charAt(i) <= '9') {
-                number = number * 10 + Integer.parseInt(String.valueOf(input.charAt(i)));
+                number = number * 10 + input.charAt(i) - '0';
             } else {
-                if (number != 0) {
-                    sum += number;
-                    number = 0;
-                }
+                sum += number;
+                number = 0;
             }
         }
         return sum + number;

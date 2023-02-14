58a89df50433ddfc87376b746bf6a662ebc4c91b
hieuneko <hieuneko@gmail.com>
2023-02-14 10:20:27 +0700

    fix solution for string 10

--- src/main/java/com/phamhieu/exercise/basic/string/String10.java
+++ src/main/java/com/phamhieu/exercise/basic/string/String10.java
@@ -4,12 +4,10 @@ public class String10 {
     public String removeUnneededSpace(final String input) {
         final StringBuilder tempString = new StringBuilder();
         for (int k = 0; k < input.length() - 1; k++) {
-            if (input.charAt(k) == ' ' && input.charAt(k + 1) == ' ') {
-                continue;
+            if (!(input.charAt(k) == ' ' && input.charAt(k + 1) == ' ')) {
+                tempString.append(input.charAt(k));
             }
-            tempString.append(input.charAt(k));
         }
-        tempString.append(input.charAt(input.length() - 1));
         return tempString.toString().trim();
     }
 }

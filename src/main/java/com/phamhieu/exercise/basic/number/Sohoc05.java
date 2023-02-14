03aaf6d7e65f395da3b6f4a3312868c50c122415
hieuneko <hieuneko@gmail.com>
2023-02-14 10:01:42 +0700

    fix solution for sohoc 05

--- src/main/java/com/phamhieu/exercise/basic/number/Sohoc05.java
+++ src/main/java/com/phamhieu/exercise/basic/number/Sohoc05.java
@@ -2,12 +2,7 @@ package com.phamhieu.exercise.basic.number;
 
 public class Sohoc05 {
 
-    //Viết phương trình nhập vào n và in ra tổng của dãy số Dãy số 1*2 + 2*3 + 3*4 +...+ n*(n+1)
     public int findSum(final int input) {
-        if (input <= 0) {
-            return 0;
-        }
-
         int total = 0;
         for (int i = 1; i <= input; i++) {
             total += i * (i + 1);
--- src/test/java/com.phamhieu.exercise.basic.number/Sohoc05Test.java
+++ src/test/java/com.phamhieu.exercise.basic.number/Sohoc05Test.java
@@ -1,15 +1,15 @@
 package com.phamhieu.exercise.basic.number;
 
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Sohoc05Test {
 
     @Test
     void findSum() {
         final var test = new Sohoc05();
-
         assertEquals(0, test.findSum(-2));
         assertEquals(0, test.findSum(0));
         assertEquals(330, test.findSum(9));

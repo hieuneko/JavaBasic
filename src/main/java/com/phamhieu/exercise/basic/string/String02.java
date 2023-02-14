4343b7c2a6ed4be1609aec9c1eaf8cf98a8f09ca
hieuneko <hieuneko@gmail.com>
2023-02-14 10:14:20 +0700

    fix solution for string 02

--- src/main/java/com/phamhieu/exercise/basic/string/String02.java
+++ src/main/java/com/phamhieu/exercise/basic/string/String02.java
@@ -2,9 +2,8 @@ package com.phamhieu.exercise.basic.string;
 
 public class String02 {
 
-    //Kiểm tra chuỗi ký tự có đối xứng hay không. (Ví dụ abcdcba là đối xứng)
     public boolean checkSymmetryString(final String input) {
-        for (int i = input.length() - 1; i >= input.length() / 2; i--) {
+        for (int i = 0; i < input.length() / 2; i++) {
             if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
                 return false;
             }

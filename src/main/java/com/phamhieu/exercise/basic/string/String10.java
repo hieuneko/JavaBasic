657ba2f38c4f5e5c5aa88faa0257dd8ecd2ff366
hieuneko <hieuneko@gmail.com>
2023-02-14 08:41:10 +0700

    fix solution for string 10

--- src/main/java/com/phamhieu/exercise/basic/string/String10.java
+++ src/main/java/com/phamhieu/exercise/basic/string/String10.java
@@ -2,27 +2,14 @@ package com.phamhieu.exercise.basic.string;
 
 public class String10 {
     public String removeUnneededSpace(final String input) {
-        int i = 0;
-        while (i < input.length() && Character.isWhitespace(input.charAt(i))) {
-            i++;
-        }
-        int j = input.length() - 1;
-        while (j >= 0 && Character.isWhitespace(input.charAt(j))) {
-            j--;
-        }
-        if (i >= j) {
-            return "";
-        }
-
-        char[] arrayInput = input.substring(i, j + 1).toCharArray();
-        String tempString = "";
-        for (int k = 0; k < arrayInput.length - 1; k++) {
-            if (arrayInput[k] == ' ' && arrayInput[k + 1] == ' ') {
+        final StringBuilder tempString = new StringBuilder();
+        for (int k = 0; k < input.length() - 1; k++) {
+            if (input.charAt(k) == ' ' && input.charAt(k + 1) == ' ') {
                 continue;
             }
-            tempString += String.valueOf(arrayInput[k]);
+            tempString.append(input.charAt(k));
         }
-        tempString += String.valueOf(arrayInput[arrayInput.length - 1]);
-        return tempString;
+        tempString.append(input.charAt(input.length() - 1));
+        return tempString.toString().trim();
     }
 }

b13520727d467da26c14e77f90d05d7284835ac5
hieuneko <hieuneko@gmail.com>
2023-02-16 13:22:26 +0700

    temp

--- src/test/java/com/phamhieu/exercise/collection/ex2/ParseJsonTest.java
+++ src/test/java/com/phamhieu/exercise/collection/ex2/ParseJsonTest.java
@@ -4,12 +4,15 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class ParseJsonTest {
 
     @Test
     void parseJson() throws IOException {
         final var parseObject = new ParseJson();
         final var vietnameseObject = parseObject.parseJson("address.json");
-        assertEquals("01", vietnameseAddress.getProvinces().get(0).getIdProvince());
+        assertEquals("01", vietnameseObject.getProvinces().get(0).getIdProvince());
+        assertEquals("Thành phố Đà Nẵng", vietnameseObject.getProvinces().get(3).getName());
     }
 }

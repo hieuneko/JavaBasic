ad0c601992f7077625a732d7fcc48e91bf5420fe
hieuneko <hieuneko@gmail.com>
2023-02-16 13:34:45 +0700

    fix oop1

--- src/main/java/com/phamhieu/exercise/oop/ex1/Utils.java
+++ src/main/java/com/phamhieu/exercise/oop/ex1/Utils.java
@@ -4,8 +4,8 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Utils {
-    
-    public double powNumber(final double a, final int n) {
+
+    public static double powNumber(final double a, final int n) {
         double result = 1;
         for (int i = 1; i <= n; i++) {
             result *= a;
@@ -13,7 +13,7 @@ public class Utils {
         return result;
     }
 
-    public int checkSidePoint(final Point a, final Point b, final Point x) {
+    public static int checkSidePoint(final Point a, final Point b, final Point x) {
         return (a.getY() - b.getY()) * (x.getX() - a.getX()) + (b.getX() - a.getX()) * (x.getY() - a.getY());
     }
 }

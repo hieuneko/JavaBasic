68fcfbd2fc0a244db8308df62539374eacd4c83c
hieuneko <hieuneko@gmail.com>
2023-02-17 08:38:39 +0700

    fix oop03

--- src/main/java/com/phamhieu/exercise/oop/ex3/Gender.java
+++ src/main/java/com/phamhieu/exercise/oop/ex3/Gender.java
@@ -1,17 +1,7 @@
 package com.phamhieu.exercise.oop.ex3;
 
 public enum Gender {
-    MALE("Male"),
-    FEMALE("Female"),
-    OTHER("Other");
-
-    private final String name;
-
-    Gender(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return this.name;
-    }
+    MALE,
+    FEMALE,
+    OTHER
 }
